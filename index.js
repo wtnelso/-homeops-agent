fcf194f8392ef21fb9b8bd45ae78eed7788b34ec
OBTRICE1658 <Oliverhbaron@gmail.com>
2025-06-07 20:45:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 dotenv.config();
 const app = express();
